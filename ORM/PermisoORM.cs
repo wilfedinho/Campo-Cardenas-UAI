134604f803cc605a9a7ff20b2d2d9ebf435fbdc5
William <william10fcb1@gmail.com>
2025-03-28 16:05:33 -0300

    Finalizacion de la capa ORM para la entidad Permiso
    La entidad Permiso incorpora el patron estructural Composite, para poder manipular de forma uniforme tanto los Permisos Simples como los Permisos Compuestos.

--- ORM/PermisoORM.cs
+++ ORM/PermisoORM.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DAO;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ORM
 {
@@ -19,9 +20,6 @@ namespace ORM
             {
                 DataTable tablaPermiso = GestorBaseDeDatos.GestorBaseDeDatosSG.DevolverTabla("Permiso");
                 DataTable tablaRelacionPermiso = GestorBaseDeDatos.GestorBaseDeDatosSG.DevolverTabla("RelacionPermiso");
-
-
-                //Recorrido para la tabla Permiso
                 DataView dvPermiso = new DataView(tablaPermiso);
                 foreach(DataRowView drv in dvPermiso) 
                 {
@@ -37,8 +35,6 @@ namespace ORM
                         ListaPermiso.Add(permisoS);
                     }
                 }
-
-                //Recorrido para la tabla PermisoRelacion
                 DataView dvRelacionPermiso = new DataView(tablaRelacionPermiso);
                 PermisoCompuesto permisoCompuestoLeido;
                 Permiso permisoLeido;
@@ -48,7 +44,6 @@ namespace ORM
                     permisoLeido = ListaPermiso.Find(x => x.obtenerPermisoNombre() == drv["permisoIncluidoNombre"].ToString());
                     permisoCompuestoLeido.Agregar(permisoLeido);
                 }
-
                 return PermisosCompuestos;
             }
             catch { PermisosCompuestos.Clear(); return ListaPermiso; }
@@ -77,7 +72,6 @@ namespace ORM
                         listaPermiso.Add(permisoS);
                     }
                 }
-
                 DataTable tablaRelacionPermiso = GestorBaseDeDatos.GestorBaseDeDatosSG.DevolverTabla("RelacionPermiso");
                 DataView dvRelacionPermiso = new DataView(tablaRelacionPermiso);
                 PermisoCompuesto compuestoLeido;
@@ -88,17 +82,11 @@ namespace ORM
                     permisoLeido = listaPermiso.Find(x => x.obtenerPermisoNombre() == drv["permisoIncluidoNombre"].ToString());
                     compuestoLeido.Agregar(permisoLeido);
                 }
-
                 PermisoLeidoGlobal = (PermisoCompuesto)permisosCompuestos.Find(x => x.obtenerPermisoNombre() == PermisoLeer);
                 return PermisoLeidoGlobal;
-
             }
             catch { permisosCompuestos.Clear(); return null; }
-
         }
-
-        //Faltan todas las variantes de leer los distintos tipos de permisos
-
         public bool InsertarPermiso(Permiso permisoNuevo, bool esRol) 
         {
             try 
@@ -108,14 +96,251 @@ namespace ORM
                 dr["tipoPermiso"] = permisoNuevo.esCompuesto() == true ? "Compuesto" : "Simple";
                 dr["esRolPermiso"] = esRol == true ? "True" : "False";
                 GestorBaseDeDatos.GestorBaseDeDatosSG.DevolverTabla("Permiso").Rows.Add(dr);
-                GestorBaseDeDatos.GestorBaseDeDatosSG.ActualizarGeneral();
+                GestorBaseDeDatos.GestorBaseDeDatosSG.ActualizarPorTabla("Permiso");
                 return true;
             }
             catch { return false; }
-        
         }
 
+        public bool InsertarRelacion(string Compuesto, string Incluido) 
+        {
+            try
+            {
+                DataRow dr = GestorBaseDeDatos.GestorBaseDeDatosSG.DevolverTabla("RelacionPermiso").NewRow();
+                dr["permisoCompuestoNombre"] = Compuesto;
+                dr["permisoIncluidoNombre"] = Incluido;
+                GestorBaseDeDatos.GestorBaseDeDatosSG.DevolverTabla("RelacionPermiso").Rows.Add(dr);
+                GestorBaseDeDatos.GestorBaseDeDatosSG.ActualizarPorTabla("RelacionPermiso");
+                return true;
+            }
+            catch { return false; }
+        }
 
+        public bool BorrarPermiso(string NombrePermiso)
+        {
+            try
+            {
+                Usuario usuarioConPermisoBorrar = UsuarioORM.GestorUsuarioORM.ObtenerUsuariosPorConsulta("Simple", "Rol", NombrePermiso)[0];
+                if (usuarioConPermisoBorrar != null)
+                {
+                    throw new Exception();
+                }
+                DataRow drEliminar = GestorBaseDeDatos.GestorBaseDeDatosSG.DevolverTabla("Permiso").Rows.Find(NombrePermiso);
+                if (drEliminar != null)
+                {
+                    drEliminar.Delete();
+                }
+                DataTable dtRelacionPermiso = GestorBaseDeDatos.GestorBaseDeDatosSG.DevolverTabla("RelacionPermiso");
+                DataView dv = new DataView(dtRelacionPermiso);
+                foreach (DataRowView drv in dv)
+                {
+                    if (NombrePermiso == drv["permisoCompuestoNombre"].ToString() || NombrePermiso == drv["permisoIncluidoNombre"].ToString())
+                    {
+                        drv.Delete();
+                    }
+                }
+                GestorBaseDeDatos.GestorBaseDeDatosSG.ActualizarPorTabla("Permiso");
+                return true;
+            }
+            catch { return false; }
+        }
+
+        public bool BorrarRelacion(string nombrePermisoCompuesto) 
+        {
+            try
+            {
+                DataTable dtRelacionPermiso = GestorBaseDeDatos.GestorBaseDeDatosSG.DevolverTabla("RelacionPermiso");
+                DataView dv = new DataView(dtRelacionPermiso);
+                foreach (DataRowView drv in dv)
+                {
+                    if (drv["permisoCompuestoNombre"].ToString() == nombrePermisoCompuesto)
+                    {
+                        drv.Delete();
+                        GestorBaseDeDatos.GestorBaseDeDatosSG.ActualizarPorTabla("Permiso");
+                    }
+                }
+                return true;
+            }
+            catch { return false; }
+        }
+
+        public bool ModificarPermiso(string nombrePermiso,string nuevoNombrePermiso) 
+        {
+            try
+            {
+                DataRow drPermiso = GestorBaseDeDatos.GestorBaseDeDatosSG.DevolverTabla("Permiso").Rows.Find(nombrePermiso);
+                if (drPermiso != null)
+                {
+                    drPermiso["nombrePermiso"] = nuevoNombrePermiso;
+                }
+                DataTable dtRelacionPermiso = GestorBaseDeDatos.GestorBaseDeDatosSG.DevolverTabla("RelacionPermiso");
+                DataView dv = new DataView(dtRelacionPermiso);
+                foreach (DataRowView drv in dv)
+                {
+                    if (drv["permisoCompuestoNombre"].ToString() == nombrePermiso)
+                    {
+                        drv["permisoCompuestoNombre"] = nuevoNombrePermiso;
+                    }
+                    if (drv["permisoIncluidoNombre"].ToString() == nombrePermiso)
+                    {
+                        drv["permisoIncluidoNombre"] = nuevoNombrePermiso;
+                    }
+                }
+                GestorBaseDeDatos.GestorBaseDeDatosSG.ActualizarPorTabla("Permiso");
+                GestorBaseDeDatos.GestorBaseDeDatosSG.ActualizarPorTabla("RelacionPermiso");
+                return true;
+            }
+            catch { return false; }
+        }
+
+        public bool esRol(string nombrePermiso) 
+        {
+            try 
+            {
+               DataRow drPermiso = GestorBaseDeDatos.GestorBaseDeDatosSG.DevolverTabla("Permiso").Rows.Find(nombrePermiso);
+               if (drPermiso["esRolPermiso"].ToString() == "True") 
+               {
+                   return true;
+               }
+               else 
+               {
+                  return false; 
+               }
+            }
+            catch { return false; }
+        }
+
+        public bool permisoExiste(string nombrePermiso) 
+        {
+            try 
+            {
+               DataRow drPermiso = GestorBaseDeDatos.GestorBaseDeDatosSG.DevolverTabla("Permiso").Rows.Find(nombrePermiso);
+               if(drPermiso != null) 
+               {
+                  return true;
+               }
+               else 
+               {
+                  return false;
+               }
+            }
+            catch { return false; }
+        }
+        #region Funciones para el Mapeo de Permisos
+
+        public List<Permiso> ObtenerPermisos() 
+        {
+            List<Permiso> ListaPermisos = new List<Permiso>();
+            DataTable dtPermiso = GestorBaseDeDatos.GestorBaseDeDatosSG.DevolverTabla("Permiso");
+            DataView dvPermiso = new DataView(dtPermiso);
+            foreach(DataRowView drv in dvPermiso) 
+            {
+                Permiso permiso; 
+                string nombrePermiso = drv["nombrePermiso"].ToString();
+                string tipoPermiso = drv["tipoPermiso"].ToString();
+                if(tipoPermiso == "Compuesto") 
+                {
+                    permiso = new PermisoCompuesto(nombrePermiso);
+                    ListaPermisos.Add(permiso);
+                }
+                else 
+                {
+                    permiso = new PermisoSimple(nombrePermiso);
+                    ListaPermisos.Add(permiso);
+                }
+            }
+            return ListaPermisos;
+        }
+
+        public List<Permiso> ObtenerPermisosSimples() 
+        {
+            List<Permiso> ListaPermisos = new List<Permiso>();
+            DataTable dtPermiso = GestorBaseDeDatos.GestorBaseDeDatosSG.DevolverTabla("Permiso");
+            DataView dvPermiso = new DataView(dtPermiso);
+            foreach (DataRowView drv in dvPermiso)
+            {
+                string tipoPermiso = drv["tipoPermiso"].ToString();
+                if (tipoPermiso == "Simple")
+                {
+                    Permiso permiso;
+                    string nombrePermiso = drv["nombrePermiso"].ToString();
+                    permiso = new PermisoSimple(nombrePermiso);
+                    ListaPermisos.Add(permiso);
+                }
+            }
+            return ListaPermisos;
+        }
+
+        public List<Permiso> ObtenerPermisosCompuestos() 
+        {
+            List<Permiso> ListaPermisos = new List<Permiso>();
+            DataTable dtPermiso = GestorBaseDeDatos.GestorBaseDeDatosSG.DevolverTabla("Permiso");
+            DataView dvPermiso = new DataView(dtPermiso);
+            foreach (DataRowView drv in dvPermiso)
+            {
+                string tipoPermiso = drv["tipoPermiso"].ToString();
+                if (tipoPermiso == "Compuesto")
+                {
+                    Permiso permiso;
+                    string nombrePermiso = drv["nombrePermiso"].ToString();
+                    permiso = new PermisoCompuesto(nombrePermiso);
+                    ListaPermisos.Add(permiso);
+                }
+            }
+            return ListaPermisos;
+        }
+
+        public List<Permiso> ObtenerRoles() 
+        {
+            List<Permiso> ListaPermisos = new List<Permiso>();
+            DataTable dtPermiso = GestorBaseDeDatos.GestorBaseDeDatosSG.DevolverTabla("Permiso");
+            DataView dvPermiso = new DataView(dtPermiso);
+            foreach (DataRowView drv in dvPermiso)
+            {
+                string nombrePermiso = drv["nombrePermiso"].ToString();
+                if(esRol(nombrePermiso)== true) 
+                {
+                  string tipoPermiso = drv["tipoPermiso"].ToString();
+                  if (tipoPermiso == "Compuesto")
+                  {
+                    Permiso permiso;
+                    permiso = new PermisoCompuesto(nombrePermiso);
+                    ListaPermisos.Add(permiso);
+                  }
+                }
+            }
+            return ListaPermisos;
+        }
+        public List<Permiso> ObtenerTodoSinRoles() 
+        {
+            List<Permiso> ListaPermisos = new List<Permiso>();
+            DataTable dtPermiso = GestorBaseDeDatos.GestorBaseDeDatosSG.DevolverTabla("Permiso");
+            DataView dvPermiso = new DataView(dtPermiso);
+            foreach (DataRowView drv in dvPermiso)
+            {
+                string nombrePermiso = drv["nombrePermiso"].ToString();
+                if (esRol(nombrePermiso) == false)
+                {
+                    string tipoPermiso = drv["tipoPermiso"].ToString();
+                    if (tipoPermiso == "Compuesto")
+                    {
+                        Permiso permiso;
+                        permiso = new PermisoCompuesto(nombrePermiso);
+                        ListaPermisos.Add(permiso);
+                    }
+                    else 
+                    {
+                        Permiso permiso;
+                        permiso = new PermisoSimple(nombrePermiso);
+                        ListaPermisos.Add(permiso);
+                    }
+                }
+            }
+            return ListaPermisos;
+
+        }
+
+        #endregion
 
     }
 }
